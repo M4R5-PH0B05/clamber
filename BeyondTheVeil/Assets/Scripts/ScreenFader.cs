9c343698d773ebc5d215710ad2c8a3371a3848e8
Morgan Bennett <153242849+M4R5-PH0B05@users.noreply.github.com>
2026-02-01 03:56:20 +0000

    Beg this works fam

---
+++ BeyondTheVeil/Assets/Scripts/ScreenFader.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenFader : MonoBehaviour
+{
+    public static ScreenFader Instance;
+
+    [SerializeField] private Image fadeImage;
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] public float fadeTime = 0.6f;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(transform.root.gameObject);
+            return;
+        }
+        Instance = this;
+
+        DontDestroyOnLoad(transform.root.gameObject);
+
+        if (!fadeImage) fadeImage = GetComponent<Image>();
+        if (!canvasGroup) canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    void Start()
+    {
+        SetAlpha(1f);
+        SetBlocking(true);
+        StartCoroutine(FadeTo(0f));
+    }
+
+    public IEnumerator FadeTo(float targetAlpha)
+    {
+        SetBlocking(true);
+
+        float startAlpha = fadeImage.color.a;
+        float t = 0f;
+
+        while (t < fadeTime)
+        {
+            t += Time.unscaledDeltaTime; // works even if timeScale = 0
+            float a = Mathf.Lerp(startAlpha, targetAlpha, t / fadeTime);
+            SetAlpha(a);
+            yield return null;
+        }
+
+        SetAlpha(targetAlpha);
+
+        if (targetAlpha <= 0.001f)
+            SetBlocking(false);
+    }
+
+    void SetBlocking(bool block)
+    {
+        if (canvasGroup)
+        {
+            canvasGroup.blocksRaycasts = block;
+            canvasGroup.interactable = block;
+        }
+        else if (fadeImage)
+        {
+            fadeImage.raycastTarget = block;
+        }
+    }
+
+    void SetAlpha(float a)
+    {
+        var c = fadeImage.color;
+        c.a = a;
+        fadeImage.color = c;
+    }
+}
