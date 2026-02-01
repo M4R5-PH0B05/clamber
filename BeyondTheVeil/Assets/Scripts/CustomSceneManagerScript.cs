d3e642411092e0e37d32a507f21711d61e0bf6f0
Morgan Bennett <153242849+M4R5-PH0B05@users.noreply.github.com>
2026-02-01 01:14:55 +0000

    Morgan monior go limp

--- BeyondTheVeil/Assets/Scripts/CustomSceneManagerScript.cs
+++ BeyondTheVeil/Assets/Scripts/CustomSceneManagerScript.cs
@@ -69,6 +69,29 @@ public class CustomSceneManagerScript : MonoBehaviour
         }
     }
 
+    public void LoadLevel3()
+    {
+        if (m_CR_LoadLevelRunning == null)
+        {
+            m_CR_LoadLevelRunning = StartCoroutine(CR_LoadScene("Level 3", m_level1SpawnPosition));
+        }
+    }
+
+    public void LoadLevel4()
+    {
+        if (m_CR_LoadLevelRunning == null)
+        {
+            m_CR_LoadLevelRunning = StartCoroutine(CR_LoadScene("Level 4", m_level1SpawnPosition));
+        }
+    }
+
+    public void LoadCredits()
+    {
+        if (m_CR_LoadLevelRunning == null)
+        {
+            m_CR_LoadLevelRunning = StartCoroutine(CR_LoadScene("Credits", m_level1SpawnPosition));
+        }
+    }
     /// <summary>
     /// Async loads given scene while performing other functions during scene loading
     /// </summary>
